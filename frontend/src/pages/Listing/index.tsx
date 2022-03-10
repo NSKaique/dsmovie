3bbc656f9f7ea3eb53ab948dd90afc6708480a91
Kaique Nunes <ikeplays14@gmail.com>
2022-03-10 20:33:08 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination"
 
-function Listing(){
-    return(
-<Pagination />
+function Listing() {
+    return (
+        <>
+            <Pagination />
+            <div className="row">
+                <div className="col">
+                <MovieCard />
+                </div>
+                <div className="col">
+                <MovieCard />
+                </div>
+                <div className="col">
+                <MovieCard />
+                </div>
+                <div className="col">
+                <MovieCard />
+                </div>
+                <div className="col">
+                <MovieCard />
+                </div>
+                <div className="col">
+                <MovieCard />
+                </div>
+            </div>
+            
+        </>
     );
 }
 
